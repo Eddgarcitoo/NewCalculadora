2f7e9451ac1a67b75703e74ac4d2a3328c43fea6
Eddgarcitoo <edchikitoo@gmail.com>
2026-02-09 17:48:47 -0600

    Subiendo mi Calculadora al nuevo repo

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -5,12 +5,14 @@ import android.widget.NumberPicker
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -25,6 +27,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.calculadora.ui.theme.CalculadoraTheme
@@ -45,24 +48,39 @@ fun Calculadora() {
     var numero1 by remember { mutableStateOf("")}
     var numero2 by remember { mutableStateOf("")}
     var resultado by remember { mutableStateOf("") }
-    Column(modifier = Modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+
+    Spacer(modifier = Modifier.height(16.dp))
+
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = "CALCULADORA"
+        )
+
+
         OutlinedTextField(
             value = numero1,
             onValueChange = { numero1 = it },
-            label = { Text("Número 1") }
+            label = { Text("Número 1") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
+
+
 
         Spacer(modifier = Modifier.height(16.dp))
 
         OutlinedTextField(
             value = numero2,
             onValueChange = { numero2 = it },
-            label = { Text("Número 2") }
+            label = { Text("Número 2") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
+
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Text(
+       Text(
             text = "Resultado: $resultado",
         )
         Spacer(modifier = Modifier.height(height = 16.dp))
@@ -73,7 +91,7 @@ fun Calculadora() {
 
                 resultado = (n1 + n2).toString()
             },
-            modifier = Modifier.fillMaxWidth()
+
         ) {
             Text("+")
         }
@@ -85,7 +103,6 @@ fun Calculadora() {
 
                 resultado = (n1 - n2).toString()
             },
-            modifier = Modifier.fillMaxWidth()
         ) {
             Text("-")
         }
@@ -98,7 +115,6 @@ fun Calculadora() {
 
                 resultado = (n1 * n2).toString()
             },
-            modifier = Modifier.fillMaxWidth()
         ) {
             Text("*")
         }
@@ -117,7 +133,7 @@ fun Calculadora() {
 
 
             },
-            modifier = Modifier.fillMaxWidth()
+
         ) {
             Text("/")
         }
